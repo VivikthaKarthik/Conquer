44c73fdd819281cee3977e58af3221f51c2bc497
Vamsi <VivikthaKarthik@gmail.com>
2024-03-13 00:18:07 +0530

    handled empty page

--- ResoClassAdmin/src/app/layout/layout.component.ts
+++ ResoClassAdmin/src/app/layout/layout.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.navigate(['/course']);
+  }
+}
