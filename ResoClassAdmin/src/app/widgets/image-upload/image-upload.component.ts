d002221f05714ddd0a80d57b2041855bfe9cb823
Vamsi <VivikthaKarthik@gmail.com>
2024-03-23 09:54:00 +0530

    Fixed Thumbnail issue

--- ResoClassAdmin/src/app/widgets/image-upload/image-upload.component.ts
+++ ResoClassAdmin/src/app/widgets/image-upload/image-upload.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-image-upload',
@@ -9,18 +16,16 @@ export class ImageUploadComponent {
   selectedFile: File | undefined;
   thumbnailUrl: string = 'assets/img/size_thumb.png';
   @Output() onFileUpload = new EventEmitter();
-  showBulkUploadButton: boolean = false;
-  @Input() imageURL : string | undefined;
+  @Input() imageURL: string | undefined;
 
-  constructor(){
-    if(this.imageURL !== undefined)
-    this.thumbnailUrl = this.imageURL;
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.imageURL !== undefined && this.imageURL !== null) {
+      this.thumbnailUrl = this.imageURL;
+    }
   }
-  
+
   onFileSelected(event: any): void {
     if (event.target.files !== undefined && event.target.files.length > 0) {
-      this.showBulkUploadButton = true;
-
       const files = event.target.files;
       if (files.length === 0) return;
 
@@ -35,8 +40,6 @@ export class ImageUploadComponent {
       };
       reader.readAsDataURL(files[0]);
       this.onFileUpload.emit(files[0]);
-    } else {
-      this.showBulkUploadButton = false;
     }
   }
 }
